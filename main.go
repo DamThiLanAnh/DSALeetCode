cd00333cf12864226b3a7006f7fca96757e7484d
DamThiLanAnh <damthilananh8@hmail.com>
2024-10-09 16:15:47 +0700

    init commit

---
+++ main.go
@@ -0,0 +1,14 @@
+package DSALeetCode
+
+import "fmt"
+
+func twoSum(nums []int, target int) []int {
+
+	return nil
+}
+
+func main() {
+	var nums = []int{2, 7, 11, 15}
+	var target = 9
+	fmt.Println(twoSum(nums, target))
+}
